8eb501a6b56e0621c0b9c2fa723735360c05bef5
frostic3 <frostic3@users.noreply.github.com>
2015-02-22 03:22:07 +0530

    Use setError for validation. Change border colour
    Change from red border to pink (Rounded_errortext)

--- app/src/main/java/com/hprotcennoc/frostic3/connectorph/UserRegForm.java
+++ app/src/main/java/com/hprotcennoc/frostic3/connectorph/UserRegForm.java
@@ -2,49 +2,30 @@ package com.hprotcennoc.frostic3.connectorph;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
-import android.widget.Switch;
-import android.widget.TextView;
 
 public class UserRegForm extends ActionBarActivity implements AdapterView.OnItemSelectedListener{
 
     private EditText name;
-    private RelativeLayout layout;
-    private TextView validation_req;
 
-    RelativeLayout.LayoutParams lParams = new RelativeLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-    );
-
+     // Validation starts here
      private View.OnFocusChangeListener mOnFocusChangeListener
             = new View.OnFocusChangeListener() {
         @Override
         public void onFocusChange(View v, boolean hasFocus) {
-            Log.i("UserRegForm","onFocusChange called");
             /* String check = name.getText().toString(); */
             if(!hasFocus){
                 if(name.length() == 0) {
-                    Log.i("UserRegForm", "Condition called");
                     name.setBackground(getResources().getDrawable(R.drawable.rounded_errortext));
-
-                    validation_req = new TextView(UserRegForm.this);
-                    validation_req.setText("Required");
-
-                    layout = (RelativeLayout) findViewById(R.id.fr_user_relative_layout);
-                    lParams.addRule(RelativeLayout.BELOW,R.id.fr_user_name_ET);
-                    layout.addView(validation_req,lParams);/**/
+                    name.setError("Required");
                 }
                 else{
-
                     name.setBackground(getResources().getDrawable(R.drawable.rounded_edittext));
+                    name.setError(null);
                 }
             }
         }
@@ -54,7 +35,6 @@ public class UserRegForm extends ActionBarActivity implements AdapterView.OnItem
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_reg_user);
-        Log.i("UserRegForm","onCreate called");
 
         name = (EditText) findViewById(R.id.fr_user_name_ET);
         name.setOnFocusChangeListener(mOnFocusChangeListener);
@@ -146,7 +126,6 @@ public class UserRegForm extends ActionBarActivity implements AdapterView.OnItem
         cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         citySpinner.setAdapter(cityAdapter);
     }
-
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
