a173be03ecd9e7d05d3968aaeca21abbca5dc14c
frostic3 <frostic3@users.noreply.github.com>
2015-03-08 00:21:25 +0530

    Add NetCheck feature before every access to database
    Add permission android.permission.ACCESS_NETWORK_STATE in Manifest file

--- app/src/main/java/com/hprotcennoc/frostic3/connectorph/OrphRegForm1.java
+++ app/src/main/java/com/hprotcennoc/frostic3/connectorph/OrphRegForm1.java
@@ -1,7 +1,10 @@
 package com.hprotcennoc.frostic3.connectorph;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -19,6 +22,10 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -125,10 +132,65 @@ public class OrphRegForm1 extends ActionBarActivity implements AdapterView.OnIte
             @Override
             public void onClick(View view) {
                 // creating new product in background thread
-                new CreateNewProduct().execute();
+                new NetCheck().execute();
             }
         });
     }
+
+    //NET CHECK STARTS HERE
+    private class NetCheck extends AsyncTask<String,String,Boolean>
+    {
+        private ProgressDialog nDialog;
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            nDialog = new ProgressDialog(OrphRegForm1.this);
+            nDialog.setMessage("Loading..");
+            nDialog.setTitle("Checking Network");
+            nDialog.setIndeterminate(false);
+            nDialog.setCancelable(true);
+            nDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... args){
+            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            if (netInfo != null && netInfo.isConnected()) {
+                try {
+                    URL url = new URL("http://www.google.com");
+                    HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+                    urlc.setConnectTimeout(3000);
+                    urlc.connect();
+                    if (urlc.getResponseCode() == 200) {
+                        return true;
+                    }
+                } catch (MalformedURLException e1) {
+                    e1.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return false;
+        }
+        @Override
+        protected void onPostExecute(Boolean th){
+
+            if(th == true){
+                nDialog.dismiss();
+                new CreateNewProduct().execute();
+            }
+            else{
+                nDialog.dismiss();
+                Toast toast = Toast.makeText(OrphRegForm1.this, "Error in Network Connection", Toast.LENGTH_LONG);
+                toast.show();
+
+            }
+        }
+    }
+    //NET CHECK ENDS HERE
+
     //DATABASE CONTINUES HERE
     /**
      * Background Async Task to Create new product
